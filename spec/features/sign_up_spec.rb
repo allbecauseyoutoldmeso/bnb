8b8d762a9d14b42ec6d2cd019efd35a4dcf153da
emmpak <eiliev05@yahoo.com>
2017-04-18 15:06:19 +0100

    Sign up validation tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,34 +1,40 @@
 feature 'User signs up' do
 
   scenario 'sign up with no error' do
-    expect { sign_up }.to change { User.count }.by 1  
+    expect { sign_up }.to change { User.count }.by 1
     expect(current_path).to eq "/apartments"
     expect(page).to have_content("Hello, Roi")
   end
 
-
-  scenario 'user enters invalid email' do
-    visit '/users/new'
-    fill_in :first_name, with: 'Sally'
-    fill_in :last_name, with: 'Wag'
-    fill_in :email, with: 'sallysally.com'
-    fill_in :password, with: 'password'
-    fill_in :password_confirmation, with: 'password'
-    click_button("Sign up")
-    expect(page).to have_content('invalid email format')
-    expect(User.count).to eq 0
-    expect(current_path).to eq '/users/new'
+  scenario 'without entering a first name' do
+    expect{ sign_up(first_name: nil) }.not_to change { User.count }
+    expect(current_path).to eq "/users"
+    expect(page).to have_content("First name must not be blank")
   end
-  scenario 'user doesn\'t enter email' do
-    visit '/users/new'
-    fill_in :first_name, with: 'Sally'
-    fill_in :last_name, with: 'Wag'
-    fill_in :password, with: 'password'
-    fill_in :password_confirmation, with: 'password'
-    click_button("Sign up")
-    expect(page).to have_content('all fields required')
-    expect(User.count).to eq 0
-    expect(current_path).to eq '/users/new'
+
+  scenario 'without entering a last name' do
+    expect{ sign_up(last_name: nil) }.not_to change { User.count }
+    expect(current_path).to eq "/users"
+    expect(page).to have_content("Last name must not be blank")
+  end
+
+  scenario 'without entering an email' do
+    expect{ sign_up(email: nil) }.not_to change { User.count }
+    expect(current_path).to eq "/users"
+    expect(page).to have_content("Email must not be blank")
+  end
+
+  scenario 'with an invalid email' do
+    expect{ sign_up(email: "roi") }.not_to change { User.count }
+    expect(current_path).to eq "/users"
+    expect(page).to have_content("Email has an invalid format")
+  end
+
+  scenario 'with an existing email' do
+    sign_up
+    expect{ sign_up }.not_to change { User.count }
+    expect(current_path).to eq "/users"
+    expect(page).to have_content("Email is already taken")
   end
 
   scenario 'mismatched passwords' do
