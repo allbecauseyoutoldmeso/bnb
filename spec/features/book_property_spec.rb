e1cd777fb6f19da319beee13273febc6d8dcb194
emmpak <eiliev05@yahoo.com>
2017-04-19 10:29:48 +0100

    Add request a property feature tests

--- spec/features/book_property_spec.rb
+++ spec/features/book_property_spec.rb
@@ -1,10 +1,22 @@
 feature 'User books a property' do
-  scenario 'select a property' do
+  xscenario 'select a property' do
     Listing.create(name: "Makers", description: "No toilets working", price: "133")
     sign_up
     click_link "Makers"
-    expect(current_path).to eq('booking/new')
+    expect(current_path).to eq('booking/Makers')
     expect(page).to have_content("Makers")
     expect(page).to have_content("Pick a night")
+  end
+
+  scenario 'request a property for certain dates' do
+    Listing.create(name: "Makers", description: "No toilets working", price: "133")
+    sign_up
+    visit('/booking/Makers')
+    fill_in :from, with: '19/04/2017'
+    fill_in :to, with: '22/04/2017'
+    click_button 'Request booking'
+    expect(current_path).to eq '/requests'
+    expect(Booking.count).to eq(1)
+    expect(page).to have_content "Makers"
   end
 end
