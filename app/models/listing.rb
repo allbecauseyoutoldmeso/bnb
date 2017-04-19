6ccf405544a10e6a7f8f86eb4e49f4ce05f2bf65
Emily Chai <echai93@gmail.com>
2017-04-19 11:04:01 +0100

    Add guard against duplicate listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,9 +2,9 @@ class Listing
   include DataMapper::Resource
 
   property :id,                   Serial
-  property :name,                 String
-  property :description,          Text
-  property :price,                Integer
+  property :name,                 String, :unique => true
+  property :description,          Text, :required => true
+  property :price,                Integer, :required => true
 
   belongs_to :user
 
--- spec/features/new_listing_spec.rb
+++ spec/features/new_listing_spec.rb
@@ -10,10 +10,18 @@ feature 'Lists a new apartment' do
   end
   scenario 'A user can add mutiple listings' do
     sign_up
+    add_property(property_name: "Scary House",
+                    description: "Boo!",
+                    price: "800")
+    expect{ add_property }.to change { Listing.count }.by 1
+    add_property
+    expect{ add_property }.to change { Listing.count }.by 1
+  end
+  scenario 'A user cannot list an existing apartment' do
+    sign_up
     add_property
     visit '/apartments'
-    add_property(property_name: "Mansion House",
-                    description: "In the heart of the countryside",
-                    price: "500")
+    add_property
+  expect{ add_property }.to change { Listing.count }.by 1
   end
 end
