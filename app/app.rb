29049ac6c56ac2401c8b7381a86f090f4a194868
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-20 15:04:36 +0100

    adds styling

--- app/app.rb
+++ app/app.rb
@@ -56,8 +56,8 @@ class Bnb < Sinatra::Base
 end
 
   get '/sessions/new' do
-    if current_user 
-      flash.now[:warning] = "You are already logged in"
+    if current_user
+      flash.now[:logged_in_warning] = "You are already logged in"
       redirect to '/apartments'
     else
     erb :'sessions/log_in'
@@ -77,7 +77,7 @@ end
 
   delete '/sessions' do
     session[:user_id] = nil
-    flash.keep[:notice] = "Successfully logged out"
+    flash.keep[:log_out_notice] = "Successfully logged out"
     redirect to '/apartments'
   end
 end
